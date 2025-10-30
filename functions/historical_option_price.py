dcb9d49dcdd9f7383bba0d89690445ccf0da7fc5
JediNakDev <pitch.jedi@gmail.com>
2025-10-30 15:38:41 +0800

    add put option and rename file

--- functions/historical_option_price.py
+++ functions/historical_option_price.py
@@ -3,9 +3,15 @@ from calc_percent_change import calc_percent_change
 from price_option import price_option
 
 
-def historical_call_option_premium(
-    symbol: str, strike_price: float, time_to_maturity_days: int
+def historical_option_price(
+    symbol: str,
+    strike_price: float,
+    time_to_maturity_days: int,
+    option_type: str = "call",
 ):
+    if option_type not in ["call", "put"]:
+        raise ValueError("option_type must be 'call' or 'put'")
+
     print("START CALCULATING HISTORICAL OPTION PRICE")
     print(f"SYMBOL: {symbol}")
     print(f"STRIKE PRICE: {strike_price}")
@@ -14,12 +20,12 @@ def historical_call_option_premium(
     current_price = price_df["Close"].iloc[-1]
     percent_change_series = calc_percent_change(price_df)
     percent_strike_price = (strike_price - current_price) / current_price
-    option_price = (
-        price_option(percent_change_series, percent_strike_price) * current_price
+    expected_payoff = price_option(
+        percent_change_series, percent_strike_price, option_type
     )
 
-    return option_price
+    return expected_payoff * current_price
 
 
 if __name__ == "__main__":
-    print(historical_call_option_premium("AAPL", 270, 1))
+    print(historical_option_price("AAPL", 240, 1, "put"))
--- functions/price_option.py
+++ functions/price_option.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 
 def price_option(
-    percent_change_series: pd.Series, percent_strike_price: float
+    percent_change_series: pd.Series, percent_strike_price: float, option_type: str
 ) -> float:
     print("START PRICING OPTION")
-    payoffs: pd.Series = (percent_change_series - percent_strike_price).where(
-        percent_change_series >= percent_strike_price, 0
+    payoffs: pd.Series = (
+        (percent_change_series - percent_strike_price).where(
+            percent_change_series >= percent_strike_price, 0
+        )
+        if option_type == "call"
+        else (percent_strike_price - percent_change_series).where(
+            percent_change_series <= percent_strike_price, 0
+        )
     )
-    print(payoffs)
     return payoffs.mean()
